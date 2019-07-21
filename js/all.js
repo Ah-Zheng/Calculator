1fdd7a6e4e7fdf14899ee8193a748147d3ffa849
jerry092383 <jerry092383@gmail.com>
2019-07-21 19:14:44 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,95 @@
+const formulaText = $('.formula');
+const cacheText = $('.cache');
+let formula = '';
+let cache = '0';
+updateView();
+
+// 數字及小數點按紐
+$(document).on('click', '.btn', function (e) {
+    e.preventDefault();
+    let num = $(this).val();
+    let formulaLast = formula.substr(-1);
+    if (formulaLast == '+' || formulaLast == '-' || formulaLast == '*' || formulaLast == '/') {
+        cache = '0';
+    }
+    if (cache == '0') {
+        if (num == '.') {
+            cache += num;
+            formula += cache;
+        } else if (num != '0') {
+            cache = '';
+            cache += num;
+            formula += num;
+        }
+    } else {
+        cache += num;
+        formula += num;
+    }
+    cacheText.val(cache);
+});
+
+// 運算符號按紐
+$(document).on('click', '.sign', function (e) {
+    e.preventDefault();
+    let sign = $(this).val();
+    if (cache != 0) {
+        let formulaLast = formula.substr(-1);
+        if (formulaLast == '+' || formulaLast == '-' || formulaLast == '*' || formulaLast == '/') {
+            formula = formula.replace(formulaLast, sign);
+        } else {
+            formula += sign;
+        }
+    } else {
+        cache += sign;
+        formula += cache;
+    }
+    formulaText.val(formula);
+});
+
+// 倒退按紐
+$('.btn_del').click(function (e) {
+    e.preventDefault();
+
+    // 判斷暫存的值是否長度為1或值為0
+    if (cache.length == 1) {
+        cache = '0';
+        formula = formula.substr(0, formula.length - 1);
+    } else {
+        // 如果不是的話就將值的最後一位字元刪除
+        cache = cache.substr(0, cache.length - 1);
+        formula = formula.substr(0, formula.length - 1);
+        console.log(formula);
+    }
+    cacheText.val(cache);
+});
+
+// 清空按紐
+$('.btn_ac').click(function (e) {
+    e.preventDefault();
+    formula = '';
+    cache = '0';
+    updateView();
+});
+
+// 等於按紐
+$('.result').click(function (e) {
+    e.preventDefault();
+    let formulaLast = formula.substr(-1);
+    if (formulaLast == '+' || formulaLast == '-' || formulaLast == '*' || formulaLast == '/') {
+        formula = formula.substr(0, formula.length - 1);
+    }
+    let res = `${eval(formula)}`;
+    if (!isNaN(res)) {
+        cache = res;
+        formula += `=${res}`;
+        updateView();
+        formula = '';
+        cache = '0';
+    }
+});
+
+// 畫面更新
+function updateView() {
+    formulaText.val(formula);
+    cacheText.val(cache);
+}
